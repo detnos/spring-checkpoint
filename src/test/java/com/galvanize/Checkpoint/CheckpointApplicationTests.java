4b53f6a7519b12037603660db3916ecdaedaf9f1
Hugh <det.nosnibor00@gmail.com>
2020-07-21 10:07:25 -0500

    initial commit

---
+++ src/test/java/com/galvanize/Checkpoint/CheckpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.Checkpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
